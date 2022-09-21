a87acc88083771991c4d9b683fce2184bb339777
artem <kalaficzkij@ukr.net>
2022-09-21 21:10:20 +0300

    last version of project(continue register)

--- data/services/validators.py
+++ data/services/validators.py
@@ -1,7 +1,19 @@
 import re
 
 
-async def validate_email(email):
-    result = await re.match("[^@]+@[^@]+\.[^@]+", email)
-    if result:
-        return True
+def validate_email(email):
+    return True if re.match("[^@]+@[^@]+\.[^@]+", email) else False
+
+
+def validate_password(password):
+    l, u, d = 0, 0, 0
+    if len(password) >= 8:
+        for i in password:
+            if i.islower():
+                l += 1
+            if i.isupper():
+                u += 1
+            if i.isdigit():
+                d += 1
+    return True if l >= 1 and u >= 1 and d >= 1 and l + u + d == len(password) else False
+
